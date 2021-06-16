fdce8389677a82681d5febdab3e5234cac41f05e
sayiO <sayionias@gmail.com>
2021-06-16 15:59:24 +0200

    java prime number project

---
+++ src/com/onayasi/Main.java
@@ -0,0 +1,17 @@
+package com.onayasi;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your number");
+        int num = sc.nextInt();
+        if(num%2 >0){
+            System.out.println("Your number " + num + " is a prime number");
+        }else{
+            System.out.println("Your number " + num + " is not a prime number");
+        }
+    }
+}
