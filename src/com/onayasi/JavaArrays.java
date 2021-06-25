67355d39f0e4c474408b96013b0487a2499717aa
sayiO <sayionias@gmail.com>
2021-06-25 21:51:46 +0200

    add all files

---
+++ src/com/onayasi/JavaArrays.java
@@ -0,0 +1,17 @@
+package com.onayasi;
+
+public class JavaArrays {
+    public static void main(String [] args){
+        int []arr = new int[5];
+        arr[0] = 26;
+        arr[1] = 27;
+        arr[2] = 56;
+        arr[3] = 78;
+        arr[4] = 41;
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i] );
+        }
+
+
+    }
+}
--- src/com/onayasi/JavaEncapsulation.java
+++ src/com/onayasi/JavaEncapsulation.java
@@ -1,16 +1,16 @@
 package com.onayasi;
 
 public class JavaEncapsulation {
-    private int acc_no;
+    private long acc_no;
     private String name;
-    private double balance;
+    private float balance;
 
 
-    public void setAcc_no(int acc_no){
+    public void setAcc_no(long acc_no){
         this.acc_no = acc_no;
     }
 
-    public void setBalance(double balance) {
+    public void setBalance(float balance) {
         this.balance = balance;
     }
     public void setName(String name){
@@ -19,16 +19,16 @@ public class JavaEncapsulation {
     public String getName(){
         return name;
         }
-    public int getAcc_no() {
+    public long getAcc_no() {
         return acc_no;
     }
-    public  double getBalance(){
+    public  float getBalance(){
         return balance;
     }
     public  static void main(String [] args){
         JavaEncapsulation c1 = new JavaEncapsulation();
         c1.setAcc_no(36459);
-        c1.setBalance(12.56);
+        c1.setBalance(1049825122);
         c1.setName("onias sayi");
         System.out.println("The account name is " + c1.getName() + " with account number " + c1.getAcc_no() + " ,having a  balance of " + c1.getBalance() );
         System.out.println(c1.getAcc_no());
