a3a82ff071ab888d8d472a63cbc1d173c436e99a
sayiO <sayionias@gmail.com>
2021-06-25 22:34:45 +0200

    add all files

--- src/com/onayasi/JavaArrays.java
+++ src/com/onayasi/JavaArrays.java
@@ -1,16 +1,20 @@
 package com.onayasi;
 
 public class JavaArrays {
-    public static void main(String [] args){
-        int []arr = new int[5];
-        arr[0] = 26;
-        arr[1] = 27;
-        arr[2] = 56;
-        arr[3] = 78;
-        arr[4] = 41;
-        for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i] );
+    int arr[];
+    static void evenValues(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            if(i%2 == 0){
+                System.out.println( arr[i] +" is at even position " + i);
+            }
         }
+        System.out.println("the number of elements in the array is " + arr.length);
+
+    }
+
+    public static void main(String [] args){
+int arr[] = {25,2,23,8,12,4,36,4585, 2000};
+evenValues(arr);
 
 
     }
